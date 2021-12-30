31a89cdb4699f0b963b810ec68338caa0d5115bb
sideghost <guilherme.cordeiro.pereira@gmail.com>
2021-12-30 12:38:40 +0000

    create BST from range not tested starting to do the graph Structure

--- src/main/kotlin/TreeUtils.kt
+++ src/main/kotlin/TreeUtils.kt
@@ -1,46 +1,50 @@
-data class Node<E>(var item: E, var left:Node<E>?, var right:Node<E>?)
+data class Node<E>(var item: E, var left: Node<E>?, var right: Node<E>?)
 
-fun <E> contains(root: Node<E>?, min:E, max:E, cmp:(e1:E, e2:E)->Int):Boolean{
-    if(root?.item == null) return false
-    if (cmp(min,root.item) <= 0 && cmp(max,root.item) >=0) return true
-    if (cmp(root.item,min) < 0 ) return contains(root.right,min,max,cmp)
-    if (cmp(max,root.item) < 0 ) return contains(root.left,min,max,cmp)
+fun <E> contains(root: Node<E>?, min: E, max: E, cmp: (e1: E, e2: E) -> Int): Boolean {
+    if (root?.item == null) return false
+    if (cmp(min, root.item) <= 0 && cmp(max, root.item) >= 0) return true
+    if (cmp(root.item, min) < 0) return contains(root.right, min, max, cmp)
+    if (cmp(max, root.item) < 0) return contains(root.left, min, max, cmp)
     return false
 }
 
 fun <E> isComplete(root: Node<E>?): Boolean {
-    return isCompleteAux(root) !=-1
+    return isCompleteAux(root) != -1
 }
 
-fun <E> isCompleteAux(root: Node<E>?):Int {
-    if(root==null) return 0
-    val l= isCompleteAux(root.left)
-    val r= isCompleteAux(root.right)
-    if(l==-1 || r==-1) return -1
-    return if(r-l == 0) l + 1 else -1
+fun <E> isCompleteAux(root: Node<E>?): Int {
+    if (root == null) return 0
+    val l = isCompleteAux(root.left)
+    val r = isCompleteAux(root.right)
+    if (l == -1 || r == -1) return -1
+    return if (r - l == 0) l + 1 else -1
 }
 
-fun createBSTFromRange(start:Int,end:Int): Node<Int>? {
+fun createBSTFromRange(start: Int, end: Int): Node<Int>? {
     //non-existing interval
     if (end - start <= 0) return null
 
     //variable initialization
-    val mid = end - (end - start)/2
-    val interval = (start .. end)
+    val mid = end - (end - start) / 2
+    val interval = (start..end)
     var leftElem = mid - 1
     var rightElem = mid + 1
-    val root = Node(mid,null,null)
+    val root = Node(mid, null, null)
     var parent = root
 
     //loop to go through every element in interval
-    while (parent.item in interval){
-        parent.left = Node(leftElem,null,null)
-        parent.right = Node(rightElem,null,null)
-        parent = parent.left
-        rightElem++
-        leftElem--
-    }
+    while (parent.item in interval) {
+        if (parent.left == null) {
+            parent.left = Node(leftElem, null, null)
+            leftElem--
+        } else {
+            parent.right = Node(rightElem, null, null)
+            rightElem++
+        }
 
+        parent = if (isComplete(parent)) parent.left!!
+        else parent.right!!
+    }
     return root
 }
 
--- src/main/kotlin/graphCollections/GraphStructure.kt
+++ src/main/kotlin/graphCollections/GraphStructure.kt
@@ -1,7 +1,39 @@
 package graphCollections
 
 class GraphStructure<I, D>: Graph<I, D> {
+    private class Vertex<I,D> :Graph.Vertex<I,D> {
+        override var data: D
+            //get() = TODO("Not yet implemented")
+        override var id: I
+            //get() = TODO("Not yet implemented")
 
+        constructor(Id: I, Data: D){
+            data = Data
+            id = Id
+        }
+
+        override fun getAdjacencies(): MutableSet<Graph.Edge<I>?> {
+            TODO("Not yet implemented")
+        }
+
+        override fun setData(newData: D): D {
+            this.data = newData
+            return newData
+        }
+
+    }
+
+    private class Edge<I> : Graph.Edge<I> {
+        override var adjacent: I
+            //get() = TODO("Not yet implemented")
+        override var id: I
+            //get() = TODO("Not yet implemented")
+
+        constructor(Adjacent: I, Id: I){
+            adjacent = Adjacent
+            id = Id
+        }
+    }
     // TO IMPLEMENT
 
     override val size: Int
